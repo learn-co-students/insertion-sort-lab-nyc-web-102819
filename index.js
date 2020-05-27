113f18dbc7fc3430e4f47e019b93ddf36461d101
Pavel Beletsky <pavel.beletsky@live.com>
2020-05-26 22:29:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 function findMinAndRemove(array){
-
+  let minIdx = 0
+  for (let i in array) {
+    if (array[minIdx] > array[i]) minIdx = i
+  }
+  return parseInt(array.splice(minIdx, 1).join())
 }
 
 function insertionSort(array){
-
+  let sorted = []
+  while (array.length) {
+    sorted.push(findMinAndRemove(array))
+  }
+  return sorted
 }
